3089b67eb73729a6310746d8358a98efffd49711
soohyun4747 <soohyun4747@gmail.com>
2025-12-12 17:06:34 +0900

    css changed

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 export const Container = ({ children }: { children: ReactNode }) => (
-  <div className="mx-auto flex min-h-screen max-w-5xl flex-col bg-gray-50 px-4 py-6 sm:px-6 lg:px-8">
+  <div className="mx-auto flex min-h-screen flex-col bg-gray-50 px-4 py-6 sm:px-6 lg:px-8">
     <div className="mx-auto w-full max-w-3xl">{children}</div>
   </div>
 );
